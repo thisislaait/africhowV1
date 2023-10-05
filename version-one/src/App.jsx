01c5eb7b14159f8a7b2f4559a0532e9e6c2e4ef2
thisislaait <ogadi.ugo@gmail.com>
2023-10-05 15:09:47 +0200

    images added

--- version-one/src/App.jsx
+++ version-one/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import React from "react";
 import Nav from "./components/Nav";
 import './App.css'
--- version-one/src/components/Nav.jsx
+++ version-one/src/components/Nav.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import hamburger from '../'
 
 const Nav = () => {
   return (
-    <div className='lg:hidden px-10 py-6 bg-black text-white '>
-        <Nav>
-            <h3 className='text-xl'>Africhow</h3>
-            <button className='bg-green-400 text-black p-4'>Get Started</button>
-        </Nav>
+    <div className='lg:hidden px-10 py-6 bg-black text-white flex justify-between'>
+      <h3 className='text-xl'>Africhow</h3>
+      <div>
+        <button className='bg-green-400 text-black p-3 font-bold'>Get Started</button>
+        <img src="" alt="" srcset="" />
+      </div>
     </div>
   )
 }
