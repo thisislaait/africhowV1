4c39f5041745b57b3f0810bb622c07a074a587f2
thisislaait <ogadi.ugo@gmail.com>
2023-10-06 12:14:40 +0200

    home page updated

--- version-one/src/components/Nav.jsx
+++ version-one/src/components/Nav.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import HamBurger from "../assets/Hamburger Menu.svg";
+import HamBurger from "../assets/hamburger.svg";
+import Avatar from "../assets/avatar.png";
 
 const Nav = () => {
   return (
-    <div className='lg:hidden px-10 py-6 bg-green-200 text-white flex justify-between items-center'>
+    <div className='lg:hidden px-10 py-6 bg-green-200 text-white flex justify-between items-center absolute top-0 right-0 left-0 z-10 bg-transparent'>
       <h3 className='text-xl font-bold text-black'>africhow</h3>
-      <div className='flex gap-6'>
-        <button className='bg-black text-green-200 py-2 px-6 font-bold'>Get Started</button>
-        <img src= {HamBurger} alt="" srcset="" className='w-10'/>
+      <div className='flex gap-6 pointer'>
+        <img src= {Avatar} alt="" srcset="" className='w-8 pointer'/>
+        <img src= {HamBurger} alt="" srcset="" className='w-8 pointer'/>
       </div>
     </div>
   )
--- version-one/src/pages/Home.jsx
+++ version-one/src/pages/Home.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import Nav from "../components/Nav";
-import hero from "../pages/Assets/Hero Image.jpg";
+import hero from "../pages/Assets/africhow banner.png";
 
 const Home = () => {
   return (
-    <div className='lg:hidden w-full bg-green-200 min-h-screen' style={{backgroundImage: `url(${hero})`}} >
+    <div className='lg:hidden w-full bg-green-200 min-h-screen bg-opacity-25'>
         <Nav />
-        <div className='snap-y snap-mandatory h-screen'>
-            <section className='h-screen flex flex-col justify-center items-center bg-cover snap-start' style={{backgroundImage: `url(${hero})`}} >
-                <h1 className='text-6xl'style={{backgroundImage: `url(${hero})`}} 
-                >Hello!</h1>
-                <button className='bg-black text-green-200 py-2 px-6 font-bold items-end'>Get Started</button>
+        <div className='snap-y snap-mandatory h-screen relative'>
+            <section className='h-screen flex flex-col snap-start justify-center items-center'>
+                <img src={hero} alt="" srcset="" className='w-full -top-8 md:hidden' />
+                <div className='text-center absolute bottom-80 px-12'>
+                    <h1 className='text-5xl font-black mt-4'>Unlock <br /> Your <span className='italic'>ideal</span> Audience </h1>
+                    <button className='bg-black text-base text-green-200 py-2 px-6 font-black mt-4 pointer'>Get Started Today</button>
+                </div>
             </section>
             
             <section className='h-screen flex justify-center items-center snap-start bg-red-200'>
                 <h3 className='bg-red-200'>Second Page</h3>
             </section>
-            <section className='h-screen flex justify-center items-center snap-start bg-red-200'>
+            <section className='h-screen flex justify-center items-center snap-start bg-orange-200'>
                 <h3 className='bg-green-200'>Third Page</h3>
             </section>
             <section className='h-screen flex justify-center items-center snap-start bg-purple-200'>
